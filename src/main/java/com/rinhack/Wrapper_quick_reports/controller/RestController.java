1a7cb4ce2c886daf2131966bc1ff21d8ca4bb1ac
ozzus <serj.reintov228@gmail.com>
2024-11-29 21:51:22 +0300

    контроллер с модельками

---
+++ src/main/java/com/rinhack/Wrapper_quick_reports/controller/RestController.java
@@ -0,0 +1,5 @@
+package com.rinhack.Wrapper_quick_reports.controller;
+
+public class RestController {
+
+}
---
+++ src/main/java/com/rinhack/Wrapper_quick_reports/models/Report.java
@@ -0,0 +1,5 @@
+package com.rinhack.Wrapper_quick_reports.models;
+
+public class Report {
+
+}

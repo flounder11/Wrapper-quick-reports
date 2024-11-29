2928d3c5159b167f82559ac4508cf6ec1817af5b
Fodi <flounder06@mail.ru>
2024-11-29 18:36:51 +0300

    Initial commit

---
+++ src/main/java/com/rinhack/Wrapper_quick_reports/WrapperQuickReportsApplication.java
@@ -0,0 +1,13 @@
+package com.rinhack.Wrapper_quick_reports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WrapperQuickReportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WrapperQuickReportsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rinhack/Wrapper_quick_reports/WrapperQuickReportsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rinhack.Wrapper_quick_reports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WrapperQuickReportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
